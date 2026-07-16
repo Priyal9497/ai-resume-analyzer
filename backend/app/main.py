77f3bb4d17cf6a07bdb9d93270cd897bf865d2ff
Priyal Aggarwal <priyal31706@gmail.com>
2026-07-16 14:51:52 +0530

    Fix frontend API endpoints paths

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://ai-resume-analyzer-three-ruddy.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
